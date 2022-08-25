f4d1d67123fca0ea4df4622f05bb3db93ed0ade2
Tony <amanzella75@gmail.com>
2022-08-25 14:30:24 -0400

    actual final styling, added home button functionality

--- client/src/class components/ClassPage.js
+++ client/src/class components/ClassPage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Student from '../student components/Student';
-import { useLocation } from 'react-router-dom'
+import { useLocation, NavLink } from 'react-router-dom'
 
 export default function ClassPage(props) {
   let location = useLocation();
@@ -9,7 +9,7 @@ export default function ClassPage(props) {
 
   const set = name => {
     return ({ target: { value } }) => {
-      setValues(oldValues => ({...oldValues, [name]: value }));
+      setValues(oldValues => ({ ...oldValues, [name]: value }));
     }
   };
 
@@ -17,15 +17,15 @@ export default function ClassPage(props) {
   const addStudent = async () => {
     const response = await fetch('/classEnrollment/enrollStudent', {
       method: 'PUT',
-      headers: {'Content-Type': "application/json; charset=utf-8"},
-      body: JSON.stringify({"studentName": values.addName, "className": location.state.className.name})
+      headers: { 'Content-Type': "application/json; charset=utf-8" },
+      body: JSON.stringify({ "studentName": values.addName, "className": location.state.className.name })
     });
     if (response.status !== 200) {
-      throw new Error(`Request failed: ${response.status}`); 
+      throw new Error(`Request failed: ${response.status}`);
     }
   }
   const onSubmitAddStudent = async (event) => {
-    event.preventDefault(); 
+    event.preventDefault();
     try {
       await addStudent();
       setValues({
@@ -38,15 +38,15 @@ export default function ClassPage(props) {
   const deleteStudent = async () => {
     const response = await fetch('/classEnrollment/dropStudent', {
       method: 'PUT',
-      headers: {'Content-Type': "application/json; charset=utf-8"},
-      body: JSON.stringify({"studentName": values.deleteName, "className": location.state.className.name})
+      headers: { 'Content-Type': "application/json; charset=utf-8" },
+      body: JSON.stringify({ "studentName": values.deleteName, "className": location.state.className.name })
     });
     if (response.status !== 200) {
-      throw new Error(`Request failed: ${response.status}`); 
+      throw new Error(`Request failed: ${response.status}`);
     }
   }
   const onSubmitDeleteStudent = async (event) => {
-    event.preventDefault(); 
+    event.preventDefault();
     try {
       await deleteStudent();
       setValues({
@@ -63,11 +63,11 @@ export default function ClassPage(props) {
       const response = await fetch(`/classEnrollment`, {
         method: 'POST',
         headers: { 'Content-Type': "application/json; charset=utf-8" },
-        body: JSON.stringify({name: location.state.className.name})
+        body: JSON.stringify({ name: location.state.className.name })
       });
       const newData = await response.json();
       console.log(newData)
-      if (newData.classInfo.classlist != null){
+      if (newData.classInfo.classlist != null) {
         setClassStudents(newData.classInfo.classlist);
       }
     };
@@ -76,20 +76,26 @@ export default function ClassPage(props) {
 
   return (
     <div className='ClassPage' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <h1 style={{ textAlign: 'center' }}> {location.state.className.name} </h1>
       <div className='content' style={{ width: '500px', justifyContent: 'center', alignItems: 'center' }}>
+        <h1 style={{ textAlign: 'center' }}> {location.state.className.name} </h1>
         {classStudents.map(c => <Student key={c.id} name={c.name} />)}
-        <div className='createDelete'>
-          <form onSubmit={onSubmitAddStudent}>
+        <div className='createDelete' style={{ width: "50%", margin: "0 auto" }}>
+          <form onSubmit={onSubmitAddStudent} style={{ width: "90%", margin: '0 auto' }}>
             <label>Name:</label>
             <input value={values.addName} onChange={set('addName')} />
             <button type="submit" onClick={() => window.location.reload(false)}>Add Student </button>
           </form>
-          <form onSubmit={onSubmitDeleteStudent}>
+          <form onSubmit={onSubmitDeleteStudent} style={{ width: "90%", margin: '0 auto' }}>
             <label>Name:</label>
             <input value={values.deleteName} onChange={set('deleteName')} />
             <button type='submit' onClick={() => window.location.reload(false)}>Remove Student </button>
           </form>
+          <br></br>
+          <button className='homePageButton' style={{ width: "90%", margin: '0 auto' }}>
+            <NavLink to={{ pathname: '/' }}>
+              <span>Home</span>
+            </NavLink>
+          </button>
         </div>
       </div>
     </div>
--- client/src/student components/StudentPage.js
+++ client/src/student components/StudentPage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Class from '../class components/Class';
-import { useLocation } from 'react-router-dom'
+import { useLocation, NavLink } from 'react-router-dom'
 
 export default function StudentPage(props) {
   let location = useLocation();
@@ -62,11 +62,11 @@ export default function StudentPage(props) {
       const response = await fetch(`/studentEnrollment`, {
         method: 'POST',
         headers: { 'Content-Type': "application/json; charset=utf-8" },
-        body: JSON.stringify({name: location.state.studentName.name})
+        body: JSON.stringify({ name: location.state.studentName.name })
       });
       const newData = await response.json();
       console.log(newData)
-      if (newData.studentInfo.enrolled != null){
+      if (newData.studentInfo.enrolled != null) {
         setStudentClasses(newData.studentInfo.enrolled);
       }
     };
@@ -75,20 +75,26 @@ export default function StudentPage(props) {
 
   return (
     <div className='StudentPage' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <h1 style={{ textAlign: 'center' }}> {location.state.studentName.name} </h1>
       <div className='content' style={{ width: '500px', justifyContent: 'center', alignItems: 'center' }}>
+        <h1 style={{ textAlign: 'center' }}> {location.state.studentName.name} </h1>
         {studentClasses.map(c => <Class key={c.id} name={c.name} />)}
-        <div className='createDelete'>
-          <form onSubmit={onSubmitAddClass}>
+        <div className='createDelete' style={{ width: "50%", margin: "0 auto" }}>
+          <form onSubmit={onSubmitAddClass} style={{ width: "90%", margin: '0 auto' }}>
             <label>Name:</label>
             <input value={values.addName} onChange={set('addName')} />
             <button type="submit" onClick={() => window.location.reload(false)}>Add Class </button>
           </form>
-          <form onSubmit={onSubmitDeleteClass}>
+          <form onSubmit={onSubmitDeleteClass} style={{ width: "90%", margin: '0 auto' }}>
             <label>Name:</label>
             <input value={values.deleteName} onChange={set('deleteName')} />
             <button type='submit' onClick={() => window.location.reload(false)}>Remove Class </button>
           </form>
+          <br></br>
+          <button className='homePageButton' style={{ width: "90%", margin: '0 auto' }}>
+            <NavLink to={{ pathname: '/' }}>
+              <span>Home</span>
+            </NavLink>
+          </button>
         </div>
       </div>
     </div>
